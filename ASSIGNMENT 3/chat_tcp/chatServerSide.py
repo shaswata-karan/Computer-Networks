41efabca0e90716a0cb4f52713376f65477fc5aa
Shaswata Karan <shaswatakaran2016@gmail.com>
2022-11-10 01:56:55 +0530

    Update chatServerSide.py

--- ASSIGNMENT 3/chat_tcp/chatServerSide.py
+++ ASSIGNMENT 3/chat_tcp/chatServerSide.py
@@ -1,4 +1,5 @@
-# Write a client-server application for chat using TCP. (Server side code)
+#Write a client-server application for chat using TCP.
+#Server side code:
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("127.0.0.1", 9090))
